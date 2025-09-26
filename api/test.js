30e825deadc4b31454e3450f6d4cd30d802d0d33
jeromtom <dev.jeromtom@gmail.com>
2025-09-26 07:54:13 +0530

    test: Add simple test endpoint

--- api/test.js
+++ api/test.js
@@ -1,16 +1,3 @@
-module.exports = function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-    
-    res.json({ 
-        message: 'Test API working!', 
-        method: req.method,
-        timestamp: new Date().toISOString()
-    });
-};
+export default function handler(req, res) {
+    res.json({ message: "API is working!", timestamp: new Date().toISOString() });
+}
